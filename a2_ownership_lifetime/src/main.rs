f832e21ff5c3923c6b2d00b4a9cc82de577c6aef
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-03-24 22:05:50 +0800

    update: 借用和所有权

--- a2_ownership_lifetime/src/main.rs
+++ a2_ownership_lifetime/src/main.rs
@@ -1,8 +1,25 @@
 fn main() {
-    let v1 = vec![1, 2, 3];
+    let mut v1 = vec![0, 1, 2];
 
-    let v2 = v1;
+    // Borrow immutably
+    // for e in v1.iter() {
+    for e in &v1 {
+        // Can also write `for e in v1.iter()`
+        println!("I'm borrowing {}.", e);
+    }
 
-    println!("{v2:?}");
-    println!("{v1:?}");
+    // Borrow mutably
+    // for e in v1.iter_mut() {
+    for e in &mut v1 {
+        *e = *e + 1;
+        println!("I'm mutably borrowing {}.", e);
+    }
+
+    // Take ownership of the whole vector
+    // for e in v1.into_iter() {
+    for e in v1 {
+        println!("I now own {}! AHAHAHAHA!", e);
+    }
+
+    // println!("{:?}", v1);
 }
