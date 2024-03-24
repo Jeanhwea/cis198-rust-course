7a190ce0927c9589d277f4be7c04dbf69af01566
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-03-24 16:31:26 +0800

    add hello world project

---
+++ a1_hello_world/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // let mut x = 1;
+    // x += 1;
+    // let y = &x;
+
+    let str1 = "hello rust";
+    let str2 = "hello rust 2".to_string();
+    println!("{str1}");
+    println!("{str2}");
+}
