0f39dbfcd5987ac50480f5e4898b7ff941818863
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-02 13:06:31 +0800

    update

--- a4_closures/src/main.rs
+++ a4_closures/src/main.rs
@@ -1,17 +1,18 @@
-struct Cacher<T>
+// 简单的缓存器
+struct Cacher<F>
 where
-    T: Fn(i32) -> i32,
+    F: Fn(i32) -> i32,
 {
-    query: T,
+    query: F,
     value: Option<i32>,
 }
 
-impl<T> Cacher<T>
+impl<F> Cacher<F>
 where
-    T: Fn(i32) -> i32,
+    F: Fn(i32) -> i32,
 {
     // 构造器
-    fn new(query: T) -> Self {
+    fn new(query: F) -> Self {
         Cacher {
             query: query,
             value: None,
@@ -19,11 +20,11 @@ where
     }
 
     // 查询接口：只缓存一次
-    fn value(&mut self, val: i32) -> i32 {
+    fn value(&mut self, arg: i32) -> i32 {
         match self.value.clone() {
             Some(v) => v,
             None => {
-                let v = (self.query)(val);
+                let v = (self.query)(arg);
                 self.value = Some(v.clone());
                 v
             }
