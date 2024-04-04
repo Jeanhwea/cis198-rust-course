aaa82421cf2561cb212dbd668f392313f02391d3
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-04 22:03:25 +0800

    update

--- a5_std_lib/src/main.rs
+++ a5_std_lib/src/main.rs
@@ -1,36 +1,15 @@
-use std::rc::Rc;
+use std::borrow::BorrowMut;
 
-// #[derive(Debug)]
-// struct Foo {
-//     v: Box<i32>,
-// }
-
-// #[derive(Debug)]
-// struct Bar {
-//     v: Rc<i32>,
-// }
+#[allow(unused_variables)]
+#[allow(dead_code)]
 
 fn main() {
     let mut v1 = Box::new(123);
-    // println!("{:p}", v1);
-    // println!("{:p}", v1.clone());
-    println!("{:?}", v1);
+    println!("v1 = {}", v1);
     *v1 = 333;
-    println!("{:?}", v1);
 
+    let v2 = v1.borrow_mut();
+    *v2 = 444;
 
-    let v2= Rc::new(222);
-    println!("{:?}", v2);
-    // println!("{:p}", v2);
-    // println!("{:p}", v2.clone());
-    // println!("{:p}", v2.clone());
-
-    // let f1 = Foo { v: v1 };
-    // let f2 = Foo { v: a };
-
-    // let v2 = Rc::new(222);
-    // let b1 = Bar { v: v2.clone() };
-    // let b2 = Bar { v: v2 };
-    // println!("{:?}", b1);
-    // println!("{:?}", b2);
+    println!("v1 = {}", v1);
 }
