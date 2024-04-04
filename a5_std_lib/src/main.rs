4daa02098a4c83bf69360f9cc0541b264c849270
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-04 19:19:35 +0800

    xxx

--- a5_std_lib/src/main.rs
+++ a5_std_lib/src/main.rs
@@ -1,12 +1,36 @@
+use std::rc::Rc;
+
+// #[derive(Debug)]
+// struct Foo {
+//     v: Box<i32>,
+// }
+
+// #[derive(Debug)]
+// struct Bar {
+//     v: Rc<i32>,
+// }
+
 fn main() {
-    let mut v1 = vec![1, 1, 1, 1];
-    for v in &mut v1 {
-        // println!("{v}");
-        *v = 2
-    }
+    let mut v1 = Box::new(123);
+    // println!("{:p}", v1);
+    // println!("{:p}", v1.clone());
+    println!("{:?}", v1);
+    *v1 = 333;
     println!("{:?}", v1);
 
-    for (i, v) in enumerate!(&v1) {
-        println!("{i}:{v}");
-    }
+
+    let v2= Rc::new(222);
+    println!("{:?}", v2);
+    // println!("{:p}", v2);
+    // println!("{:p}", v2.clone());
+    // println!("{:p}", v2.clone());
+
+    // let f1 = Foo { v: v1 };
+    // let f2 = Foo { v: a };
+
+    // let v2 = Rc::new(222);
+    // let b1 = Bar { v: v2.clone() };
+    // let b2 = Bar { v: v2 };
+    // println!("{:?}", b1);
+    // println!("{:?}", b2);
 }
