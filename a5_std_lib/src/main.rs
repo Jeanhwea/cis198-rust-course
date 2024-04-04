28036aa2acee36ca91b9ed24173c9de38085b61a
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-04 23:20:05 +0800

    update

--- a5_std_lib/src/main.rs
+++ a5_std_lib/src/main.rs
@@ -1,13 +1,17 @@
+use std::rc::Rc;
+
 #[allow(unused_variables)]
 #[allow(dead_code)]
 
 fn main() {
-    let mut v1 = Box::new(123);
-    println!("v1 = {}", v1);
-    *v1 = 333;
+    // let mut v1 = Box::new(123);
+    // *v1 = 222;
+    // println!("v1 = {}", v1);
 
-    let v2 = &mut *v1;
-    *v2 = 444;
-
-    println!("v1 = {}", v1);
+    let mut v2 =  Rc::new(123);
+    // *v2 = 222;
+    let r1 = Rc::get_mut(&mut v2);
+    println!("r1 = {:?}", r1);
+    // *r1.unwrap() = 444;
+    // println!("v2 = {}", v2);
 }
