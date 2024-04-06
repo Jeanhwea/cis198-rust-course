c0a62a6c33512aa48d2f5c2506da06ed8fa163f8
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-06 14:11:16 +0800

    update: 添加 Cell/RefCell

--- a5_std_lib/src/main.rs
+++ a5_std_lib/src/main.rs
@@ -1,17 +1,20 @@
-use std::rc::Rc;
+use std::cell::{Cell, RefCell};
 
 #[allow(unused_variables)]
 #[allow(dead_code)]
+#[derive(Debug)]
+struct Foo {
+    x: Cell<i32>,
+    y: RefCell<u32>,
+}
 
 fn main() {
-    // let mut v1 = Box::new(123);
-    // *v1 = 222;
-    // println!("v1 = {}", v1);
+    let v1 = RefCell::new(vec![1]);
 
-    let mut v2 =  Rc::new(123);
-    // *v2 = 222;
-    let r1 = Rc::get_mut(&mut v2);
-    println!("r1 = {:?}", r1);
-    // *r1.unwrap() = 444;
-    // println!("v2 = {}", v2);
+    let mut inner = v1.borrow_mut();
+    inner.push(2);
+
+    println!("v1 = {:?}", v1.borrow());
+
+    let mut inner2 = v1.borrow();
 }
