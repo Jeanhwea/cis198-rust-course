24f1bc2ecc9e8a5374a24d005a12047ef136503c
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-06 22:04:55 +0800

    add

--- a6_misc/src/main.rs
+++ a6_misc/src/main.rs
@@ -1,6 +1,16 @@
+// extern crate rand;
+// use rand::Rng;
+
+use rand::Rng;
+
 // use a6_misc::english::greetings;
 use a6_misc::chinese::greetings;
 
 fn main() {
     greetings::say();
+
+    let mut r = rand::thread_rng();
+    for _ in 1..=3 {
+        println!("rand = {}", r.gen::<u8>());
+    }
 }
