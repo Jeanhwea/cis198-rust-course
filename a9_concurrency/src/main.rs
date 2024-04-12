86bbd099408bd33fb35ec21b579e22881e9b843e
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-12 22:23:38 +0800

    xx

--- a9_concurrency/src/main.rs
+++ a9_concurrency/src/main.rs
@@ -1,10 +1,22 @@
-use std::sync::Once;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::{hint, thread};
 
 fn main() {
-    static INIT: Once = Once::new();
-    for _ in 0..10 {
-        INIT.call_once(|| {
-            println!("xxx");
-        });
+    let spinlock = Arc::new(AtomicUsize::new(1));
+
+    let spinlock_clone = Arc::clone(&spinlock);
+
+    let thread = thread::spawn(move || {
+        spinlock_clone.store(0, Ordering::Release);
+    });
+
+    // Wait for the other thread to release the lock
+    while spinlock.load(Ordering::Acquire) != 0 {
+        hint::spin_loop();
+    }
+
+    if let Err(panic) = thread.join() {
+        println!("Thread had an error: {panic:?}");
     }
 }
