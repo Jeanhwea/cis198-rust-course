c22109c224ad47ed68a539affb3b1e8bee140477
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-13 21:25:20 +0800

    xxx

--- a9_concurrency/src/main.rs
+++ a9_concurrency/src/main.rs
@@ -1,4 +1,17 @@
+fn inc(slice: &mut [i32]) {
+    if slice.len() < 100 {
+        for p in slice {
+            *p += 1;
+        }
+    } else {
+        let mid = slice.len() / 2;
+        let (left, right) = slice.split_at_mut(mid);
+        rayon::join(|| inc(left), || inc(right));
+    }
+}
+
 fn main() {
-    let x = vec![1, 2, 3];
+    let mut x = vec![1; 3000];
+    inc(&mut x);
     println!("x = {:?}", x);
 }
