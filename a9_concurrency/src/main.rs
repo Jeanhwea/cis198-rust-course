e20fdeade300f71b647588347b00bbc104efb2a4
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-13 21:03:41 +0800

    xxx

--- a9_concurrency/src/main.rs
+++ a9_concurrency/src/main.rs
@@ -1,22 +1,4 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
-use std::{hint, thread};
-
 fn main() {
-    let spinlock = Arc::new(AtomicUsize::new(1));
-
-    let spinlock_clone = Arc::clone(&spinlock);
-
-    let thread = thread::spawn(move || {
-        spinlock_clone.store(0, Ordering::Release);
-    });
-
-    // Wait for the other thread to release the lock
-    while spinlock.load(Ordering::Acquire) != 0 {
-        hint::spin_loop();
-    }
-
-    if let Err(panic) = thread.join() {
-        println!("Thread had an error: {panic:?}");
-    }
+    let x = vec![1, 2, 3];
+    println!("x = {:?}", x);
 }
