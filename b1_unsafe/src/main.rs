4d3e680556705c83bf2d647b050d738b2d8e9533
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-15 21:55:36 +0800

    xx

--- b1_unsafe/src/main.rs
+++ b1_unsafe/src/main.rs
@@ -1,6 +1,8 @@
+#[link(name = "foo", kind = "dylib")]
+extern "C" {
+    fn foo() -> i32;
+}
+
 fn main() {
-    // let v1 = vec![1,2,3];
-    let mut v1 = Vec::<i32>::new();
-    v1.insert(0, 3);
-    println!("v1 = {v1:?}");
+    println!("foo() = {:?}", unsafe { foo() });
 }
