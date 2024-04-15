10d35c52c1cb222ac7d3f95f141d4c5746916eb2
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-15 22:28:39 +0800

    xxx

--- b1_unsafe/src/main.rs
+++ b1_unsafe/src/main.rs
@@ -1,9 +1,8 @@
-#[link(name = "libffi.so", kind = "dylib")]
+#[link(name = "ffi", kind = "dylib")]
 extern "C" {
     fn foo() -> i32;
 }
 
 fn main() {
     println!("foo() = {:?}", unsafe { foo() });
-    println!("Hello");
 }
