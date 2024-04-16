debd946d981fb3600e00fbb8d6331ab568d51a74
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-16 13:39:06 +0800

    update

--- b2_macro/src/main.rs
+++ b2_macro/src/main.rs
@@ -1,3 +1,13 @@
+macro_rules! incr {
+    ($x:ident) => {
+        $x += 1;
+    };
+}
+
 fn main() {
-    println!("Hello");
+    let mut x = 0;
+
+    incr!(x); // x += 1;
+
+    println!("x = {x}");
 }
