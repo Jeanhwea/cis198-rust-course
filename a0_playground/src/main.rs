326adb9d2a826c716e8fc1cb947bef286b0b7c29
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-18 21:49:23 +0800

    xx

--- a0_playground/src/main.rs
+++ a0_playground/src/main.rs
@@ -2,24 +2,18 @@
 #![allow(dead_code)]
 #![allow(unused_mut)]
 
-use std::mem::transmute;
-
-#[derive(Debug)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-fn foo() -> i32 {
-    123
+fn longest<'a>(s: &'a str, t: &'a str) -> &'a str {
+    if s.len() > t.len() {
+        s
+    } else {
+        t
+    }
 }
 
 fn main() {
-    // let pointer = foo as *const ();
-    // let f1 = unsafe { std::mem::transmute::<*const (), fn() -> i32>(pointer) };
-    // println!("ans = {:?}", f1());
+    let s1 = String::from("abcd");
+    let s2 = "xyz";
 
-    let mut v1 = vec![1, 2, 3];
-    v1.insert(1, 99);
-    println!("v1 = {:?}", v1);
+    let s3 = longest(s1.as_str(), s2);
+    println!("s3 = {:?}", s3);
 }
