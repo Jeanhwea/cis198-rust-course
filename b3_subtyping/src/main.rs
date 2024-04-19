c3c996e9a9b8e8cce7e076d1cde986337b4a0815
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-04-19 21:47:15 +0800

    xxx

---
+++ b3_subtyping/src/main.rs
@@ -0,0 +1,25 @@
+struct Closure<F> {
+    data: (u8, u16),
+    func: F,
+}
+
+impl<F> Closure<F>
+where
+    F: Fn(&(u8, u16)) -> &u8,
+{
+    fn call(&self) -> &u8 {
+        (self.func)(&self.data)
+    }
+}
+
+fn do_it(data: &(u8, u16)) -> &u8 {
+    &data.0
+}
+
+fn main() {
+    let c = Closure {
+        data: (12, 3),
+        func: do_it,
+    };
+    println!("ans = {}", c.call());
+}
